0149f9704c8c5f7c4d97ad53206ecc79a2a17de3
廖建军 <liaojianjun@neotel.com.cn>
2016-12-19 18:26:47 +0800

    index

--- public/app/pages/Index/index.js
+++ public/app/pages/Index/index.js
@@ -7,6 +7,8 @@ import {browserHistory} from 'react-router';
 import {Input,Button,Icon} from 'antd'
 const Search=Input.Search;
 
+import './index.scss'
+
 class Index extends Component{
   constructor(props){
     super(props);
@@ -23,13 +25,18 @@ class Index extends Component{
     return(
       <div className="container">
         <div className="sign">
-          <Search placeholder="请输入您要查找的内容" onSearch={value=>console.log(value)} style={{paddingRight:10}}/>
+          <Search placeholder="请输入您要查找的内容" onSearch={value=>console.log(value)}/>
           <Button.Group size="default">
             <Button type="primary"><Icon type="user" onClick={()=>browserHistory.push('/login')}/>登录</Button>
             <Button onClick={()=>browserHistory.push('/')}>注册</Button>
           </Button.Group>
         </div>
-
+        <div className="title">
+          <div className="img"></div>
+          <div className="name">
+          <h2>儿科医师培训平台</h2>
+          </div>
+        </div>
       </div>
     )
   }
